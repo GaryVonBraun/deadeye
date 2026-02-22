fb6c2b323d3a2800fd3998327c6943c33d23739b
Gary <garyvonbraun@gmail.com>
2026-02-22 15:21:22 +0100

    added the collision plugin to simulation

--- src/simulation/mod.rs
+++ src/simulation/mod.rs
@@ -1,10 +1,12 @@
 use bevy::prelude::*;
 
+use crate::simulation::collision::CollisionPlugin;
+
 pub mod collision;
 pub struct SimulationPlugin;
 
 impl Plugin for SimulationPlugin {
-   fn build(&self, _app: &mut App) {
-       
+   fn build(&self, app: &mut App) {
+       app.add_plugins(CollisionPlugin);
    } 
 }
