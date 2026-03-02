8b65389db0abee7f986cfecec01fe72767ba43cf
Gary <garyvonbraun@gmail.com>
2026-03-02 20:54:43 +0100

    Ai chasing is now set to keep distance and follow

--- src/ai/systems.rs
+++ src/ai/systems.rs
@@ -1,6 +1,12 @@
 use bevy::prelude::*;
 
-use crate::{actor::{components::Actor, locomotion::components::AiMovementIntent}, ai::components::{AiController, AiIntent}};
+use crate::{
+    actor::{components::Actor, locomotion::components::AiMovementIntent},
+    ai::{
+        components::{AiController, AiIntent},
+        vision::components::Vision,
+    },
+};
 
 pub fn ai_targeting_system(
     mut ai_query: Query<(Entity, &Transform, &mut AiController)>,
@@ -30,16 +36,39 @@ pub fn ai_targeting_system(
 }
 
 pub fn ai_movement_system(
-    mut ai_query: Query<(&AiController, &Transform, &mut AiMovementIntent)>,
+    mut ai_query: Query<(&AiController, &Transform, &mut AiMovementIntent, &Vision)>,
     actor_query: Query<&Transform, With<Actor>>,
 ) {
-    for (controller, ai_transform, mut movement_intent) in ai_query.iter_mut() {
+    for (controller, ai_transform, mut movement_intent, ai_vision) in ai_query.iter_mut() {
         match controller.black_board.intent {
             AiIntent::Chase(target) => {
                 if let Ok(target_transform) = actor_query.get(target) {
-                    let direction = (target_transform.translation.truncate() 
-                        - ai_transform.translation.truncate()).normalize();
-                    movement_intent.move_direction = direction;
+                    let distance = Vec2::distance(
+                        target_transform.translation.truncate(),
+                        ai_transform.translation.truncate(),
+                    );
+
+                    let keep_distance_at = ai_vision.range / 2.;
+
+                    //TEMPORARY - this is not good code and should be fixed
+                    //FIXME - this is a mess
+
+                    if distance > keep_distance_at {
+                        // go towards target
+                        let direction = (target_transform.translation.truncate()
+                            - ai_transform.translation.truncate())
+                        .normalize();
+                        movement_intent.move_direction = direction;
+                    } else if distance == keep_distance_at {
+                        // don't more
+                        movement_intent.move_direction = Vec2::default();
+                    } else {
+                        // go in opposite direction
+                        let direction = (ai_transform.translation.truncate()
+                            - target_transform.translation.truncate())
+                        .normalize();
+                        movement_intent.move_direction = direction;
+                    }
                 }
             }
             _ => movement_intent.move_direction = Vec2::ZERO,
